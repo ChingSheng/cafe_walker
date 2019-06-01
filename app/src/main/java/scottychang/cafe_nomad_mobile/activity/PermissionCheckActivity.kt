80d31c192ad4563f236196a720adc397ded517cc
scott <scott@sixnology.com>
2019-06-01 16:32:59 +0800

    Adjust permission check (Include UI & remove unnecessary permission)

--- app/src/main/java/scottychang/cafe_nomad_mobile/activity/PermissionCheckActivity.kt
+++ app/src/main/java/scottychang/cafe_nomad_mobile/activity/PermissionCheckActivity.kt
@@ -6,16 +6,17 @@ import android.os.Handler
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import scottychang.cafe_nomad_mobile.R
 
 class PermissionCheckActivity : AppCompatActivity() {
     private val PERMISSION_ALL = 1
     private val PERMISSIONS = arrayOf(
-        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
         android.Manifest.permission.ACCESS_FINE_LOCATION
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.setBackgroundResource(R.drawable.background)
         if (!hasPermissions()) {
             ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL)
         } else {
@@ -36,7 +37,7 @@ class PermissionCheckActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         for (grantResult:Int in grantResults) {
             if (grantResult != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission not granted", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, R.string.permission_not_grant, Toast.LENGTH_LONG).show()
                 Handler().postDelayed({finish()}, 3000)
                 return
             }
