f303b1bb750cbc946ce2febd8bcc9847276d987a
scott <mmm90415@gmail.com>
2019-06-30 15:48:42 +0800

    Remove unused ui and events

--- app/src/main/java/scottychang/cafe_walker/activity/MapActivity.kt
+++ app/src/main/java/scottychang/cafe_walker/activity/MapActivity.kt
@@ -169,11 +169,9 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             when (newState) {
                 BottomSheetBehavior.STATE_EXPANDED -> {
                     bottomSheetIndicator.setImageResource(R.drawable.down)
-                    bottomSheetTitle.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.sort_down, 0)
                 }
                 BottomSheetBehavior.STATE_COLLAPSED -> {
                     bottomSheetIndicator.setImageResource(R.drawable.up)
-                    bottomSheetTitle.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.sort_up, 0)
                 }
             }
         }
